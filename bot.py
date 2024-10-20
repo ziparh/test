9433cb9f5bb9500c7eb0251010d4c82c464bf5b8
ziparh <thelackutop@gmail.com>
2024-10-20 12:42:18 +0200

    3

--- bot.py
+++ bot.py
@@ -7,6 +7,16 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    chat_id = message.chat.id
+    user_id = message.reply_to_message.from_user.id
+    user_status = bot.get_chat_member(chat_id, user_id).status 
+    if message.text.startswith('https://'):
+        bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+        bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    
+
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 

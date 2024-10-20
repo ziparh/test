cda42165d02b952ebfa19562568781bfdfcdd1a8
ziparh <thelackutop@gmail.com>
2024-10-20 12:48:38 +0200

    4

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     chat_id = message.chat.id
@@ -24,7 +29,8 @@ def ban_user(message):
          # сохранение id и статуса пользователя, отправившего сообщение
         user_id = message.reply_to_message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+         # проверка пользователяgit add .
+
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-token = "Tokenfff" # токен для бота f
-=======
-token = "7955444251:AAHt4wwyTPUUvOwhhT_UVw0hdMq7X_O04Lk" # токен для бота f
->>>>>>> 6206418 (1)
+token = "" # токен для бота
